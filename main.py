fefc0e76ff4552b0b4c27232881a26da714cba8e
Sirajuddin Asjad <sirasjad@gmail.com>
2017-08-24 13:22:17 +0200

    Update

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ pygame.init()
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption(title)
 
-bg = pygame.image.load("loading.png")
-bg = bg.convert_alpha()
-bgRect = bg.get_rect()
-screen.blit(bg, bgRect)
-pygame.display.flip()
-time.sleep(3)
+# bg = pygame.image.load("loading.png")
+# bg = bg.convert_alpha()
+# bgRect = bg.get_rect()
+# screen.blit(bg, bgRect)
+# pygame.display.flip()
+# time.sleep(3)
+
+
 RED = (255,0,0)
 screen.fill(RED)
-pygame.display.flip()
-
 car = pygame.image.load("car.png")
 car = car.convert_alpha()
 carRect = car.get_rect()
+carRect.x = 400
+carRect.y = 600
 screen.blit(car, carRect)
 pygame.display.flip()
-print("hello")
