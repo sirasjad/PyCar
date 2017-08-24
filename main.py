6eb1de19349465734b9fd982b5c73d2c89300bd3
Sirajuddin Asjad <sirasjad@gmail.com>
2017-08-24 10:33:54 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# PyCar | Developed by Sirajuddin Asjad and Kirisan Manivannan
+
+# 
+
+print(test)
---
+++ modules.py
@@ -0,0 +1,3 @@
+# PyCar | Developed by Sirajuddin Asjad and Kirisan Manivannan
+
+test = "this is a test"
